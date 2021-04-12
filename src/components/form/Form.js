7562d344b642712eb004c53e084193f35b023ad6
Kristen Bair <kristenmbair@gmail.com>
2021-04-12 12:07:35 -0600

    Create initial form inputs and state hooks

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,16 +2,46 @@ import React, {useState} from 'react'
 import './Form.css';
 
 const Form = () => {
-  const [name, setName] = useState("Name");
+  const [venueName, setVenueName] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [date, setDate] = useState('');
+
+
+
 
   return (
     <div className="Form">
-      <h1>THIS IS THE Form</h1>
+      <h1>Create new event</h1>
       <form>
         <input
-          type="text"
-          name="eventName"
-          />
+          type='text'
+          placeholder='Venue Name'
+          name='venueName'
+          value={venueName}
+          onChange={event => setVenueName(event.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='City'
+          name='city'
+          value={city}
+          onChange={event => setCity(event.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='State'
+          name='state'
+          value={state}
+          onChange={event => setState(event.target.value)}
+        />
+        <input
+          type='date'
+          placeholder='Date'
+          name='date'
+          value={date}
+          onChange={event => setDate(event.target.value)}
+        />
       </form>
     </div>
   );
