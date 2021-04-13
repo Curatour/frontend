81f7f4f435c4221471f34c9096a1497bb1601b09
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-04-12 19:10:42 -0600

    Add code annotations for API call events

--- src/components/calendar/event-utils.js
+++ src/components/calendar/event-utils.js
@@ -1,10 +1,17 @@
 let eventGuid = 0
 const today = new Date()
-let todayStr = today.toLocaleDateString('pt-BR').split('/').reverse().join('-')
+let todayStr = today.toLocaleDateString("pt-br").split('/').reverse().join('-')
+
+//2021-04-12 - from input form -- will already be formated correctly
+
+console.log(todayStr)
 
 const insertTimeHere = "T12:00:00"
 
 export const INITIAL_EVENTS = [
+  //API CALL - get EVENTS => response (CONTEXT)
+  //import useApp from '../../context/appProvider'
+  //const {events} = useApp()
   {
     id: createEventId(),
     title: 'All-day event',
@@ -18,5 +25,6 @@ export const INITIAL_EVENTS = [
 ]
 
 export function createEventId() {
+  console.log(todayStr)
   return String(eventGuid++)
 }
