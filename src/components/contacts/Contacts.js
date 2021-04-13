fb3c19e051123e573650694165504e134bc6745a
Richard Tyler <richardtyler2011@gmail.com>
2021-04-13 14:11:38 -0600

    Make dummy contacts data

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+const contacts = [
+  {
+    firstName: 'Kevin',
+    lastName: 'Hartmann',
+    email: 'ilikeadasauce55@aim.com',
+    phone: '(203) 889-6969'
+  }, {
+    firstName: 'Drew',
+    lastName: 'Bradley',
+    email: 'drewbreezey420@hotmail.com',
+    phone: '(420) 123-4567'
+  }, {
+    firstName: 'Kristen',
+    lastName: 'Bair',
+    email: 'kristenbibbles@gamil.com',
+    phone: '(469) 109-8765'
+  }, {
+    firstName: 'Richard',
+    lastName: 'Tyler',
+    email: 'richardrhinosaurus@hotmail.com',
+    phone: '(897) 835-5947'
+  }];
+
 const Contacts = () => {
   return (
     <div className='Contacts'>
+
     </div>
   )
 }
