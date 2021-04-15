142d4527a662d0d0197d3b305868e3e2d2e30403
Richard Tyler <richardtyler2011@gmail.com>
2021-04-15 16:33:35 -0600

    Display inputs for add contact

--- src/components/contacts/AddContacts.js
+++ src/components/contacts/AddContacts.js
@@ -1,8 +1,35 @@
 import React, { useState, useRef } from 'react';
 
 const AddContacts = () => {
+  const nameRef = useRef();
+  const phoneRef = useRef();
+  const emailRef = useRef();
+
   return (
-    null
+    <form className='add-contacts'>
+      <input 
+        type='text'
+        name='contact-name'
+        placeholder='Contact Name'
+        ref={nameRef}
+      />
+
+      <input 
+        type='text'
+        name='contact-phone'
+        placeholder='Contact Phone Number'
+        ref={phoneRef}
+      />
+
+      <input 
+        type='text'
+        name='contact-email'
+        placeholder='Contact Email'
+        ref={emailRef}
+      />
+
+      <button>Add Contact</button>
+    </form>
   )
 }
 
