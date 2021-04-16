5c468f1d81b4c28cd98cbbf63f80db86216ef860
Drew Bradley <williamandrewb@gmail.com>
2021-04-15 20:58:21 -0600

    Update styling.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -39,23 +39,23 @@ const Dashboard = () => {
               events={[
               {
                 title: 'Meeting',
-                start: '2021-04-12T14:30:00',
+                start: '2021-04-17T14:30:00',
               },
               {
                 title: 'Birthday Party',
-                start: '2021-04-13T07:00:00',
+                start: '2021-04-18T07:00:00',
               },
               {
                 title: 'Big Concert',
-                start: '2021-04-13T16:00:00',
+                start: '2021-04-19T16:00:00',
               },
               {
                 title: 'Photo Shoot',
-                start: '2021-04-14T16:00:00',
+                start: '2021-04-20T16:00:00',
               },
               {
                 title: 'TV Interview',
-                start: '2021-04-15T09:00:00',
+                start: '2021-04-22T09:00:00',
               }
             ]}
           />
