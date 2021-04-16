33f417ea84e384b779238cbc6735b2a00d4c5e91
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-04-15 20:17:45 -0600

    Add strikethrough to completed

--- src/components/agenda/AgendaItem.js
+++ src/components/agenda/AgendaItem.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function AgendaItem({info}) {
   const {id, startTime, title, description, isCompleted} = info
   const [completed, setCompleted] = useState(isCompleted)
 
+  useEffect(() => {
+    //INSERT USECONTEXT TO UPDATE SUBEVENT ITEM
+  }, [completed])
+
   return (
     <div id={id} className='agenda-item'>
       <div className='title-wrapper'>
         <input className='agenda-check'type='checkbox' checked={completed} onChange={() => setCompleted(completed ? false : true)}/>
-        <p className='agenda-item-title'>{startTime} - {title}</p>    
+        <p className={`agenda-item-title ${completed ? 'completed' : null}`}>{startTime} - {title}</p>    
       </div>
-      <p className='agenda-item-desc'>{description}</p>
+      <p className={`agenda-item-desc ${completed ? 'completed' : null}`}>{description}</p>
     </div>
   );
 }
