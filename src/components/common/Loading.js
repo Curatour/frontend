43d7684a6b92faef2fee7cde47c4b697d58de384
Richard Tyler <richardtyler2011@gmail.com>
2021-04-17 11:14:24 -0600

    CHange Loading color to the light grey

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCompactDisc } from '@fortawesome/free-solid-svg-icons'
+import './common.css';
 
 const Loading = () => {
   return (
-    <div className='loading-container'>
+    <div className='Loading-container'>
       <h2 className='Loading-heading'>Loading...</h2>
       <FontAwesomeIcon icon={faCompactDisc} className='fa-spin' size='5x' />
     </div>
