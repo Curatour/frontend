90233a7194e6f6dfa0756096be7da07e30cd1ce6
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-04-17 09:16:53 -0600

    fix merge conflict with stashed changes

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -9,29 +9,59 @@ export const useApp = () => {
 }
 
 const AppProvider = ({children}) => {
-  // global useState
-  const tourData = useQuery(TOURS_QUERY)
-  const [tours, setTours] = useState(tourData.data ? tourData.data.tours : [])
 
+  // queries
+  const toursData = useQuery(TOURS_QUERY)
   const eventsData = useQuery(EVENTS_QUERY)
+  const contactsData = useQuery(CONTACT_QUERY)
+  
+  // mutations
+    
+  
+  // GLOBAL STATE
+  // intial data -> state match queries
+  const [allTours, setTours] = useState(toursData.data ? toursData.data.tours : [])
   const [events, setEvents] = useState(eventsData.data ? eventsData.data.events : [])
-  console.log(eventsData)
-
+  const [venues, setVenues] = useState([])
   const [contacts, setContacts] = useState([])
-
+  
+  // generic state info
   const [error, setError] = useState('')
   const [loading, setLoading] = useState('')
   const [message, setMessage] = useState('')
 
-  // functions
+  // FUNCTIONS
+  const updateTours = (newTour) => {
+    setTours([...allTours, newTour])
+    //setLoading, setError
+    //ADD MUTATIONS TO update
+    //resetLoading, resetError
+  }
 
-  // const getEvents = () => {
-    // do API call
-    // apiCall.getEvents
-    // return what we want
-  // }
+  const updateEvents = (newEvent) => {
+    setEvents([...events, newEvent])
+    //setLoading, setError
+    //ADD MUTATIONS TO update
+    //resetLoading, resetError
+  }
 
-  // values
+  const updateVenues = (newVenue) => {
+    setVenues([...venues, newVenue])
+    
+    //setLoading, setError
+    //ADD MUTATIONS
+    //resetLoading, resetError
+  }
+
+  const updateContacts = (newContact) => {
+    setContacts([...contacts, newContact])
+    //setLoading, setError
+    //ADD MUTATIONS
+    //resetLoading, resetError
+  }
+
+
+  // VALUES
 
   const value = {
     tours, 
