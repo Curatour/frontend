d8e37c778eaefc19f6b9b9d126de883b740fe4f2
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-04-18 11:07:30 -0600

    update mutation fuinction

--- src/context/mutations.js
+++ src/context/mutations.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const EVENTS_MUTATION = gql`
-   mutation CreateEvent($input: CreateEventInput! ) {
+   mutation CreateEvent($input: CreateEventInput!) {
     mutateEvent(input: $input) {
       venueId,
       name, 
