8fb37d0d102262bf819809e9a5ab03bfd1168eaa
Richard Tyler <richardtyler2011@gmail.com>
2021-04-19 11:59:20 -0600

    Test each contact card

--- cypress/integration/add_contacts_spec.js
+++ cypress/integration/add_contacts_spec.js
@@ -25,28 +25,28 @@ describe('AddContacts', () => {
     cy.get('.add-contacts input').should('have.length', 4);
   });
 
-  // it('should add a contact to the list', () => {
-  //   cy.get('.ContactCard').should('have.length', 4);
+  it('should add a contact to the list', () => {
+    cy.get('.ContactCard').should('have.length', 4);
 
-  //   cy.get('.add-contacts-button').click();
-  //   cy.get('.add-contacts input:first').type('Gavin');
-  //   cy.get('.add-contacts input').eq(1).type('Belson');
-  //   cy.get('.add-contacts input').eq(2).type('gavin.belson@hooli.com');
-  //   cy.get('.add-contacts input:last').type('(303) 303-3030');
-  //   cy.get('.module-buttons button:first').click();
+    cy.get('.add-contacts-button').click();
+    cy.get('.add-contacts input:first').type('Gavin');
+    cy.get('.add-contacts input').eq(1).type('Belson');
+    cy.get('.add-contacts input').eq(2).type('gavin.belson@hooli.com');
+    cy.get('.add-contacts input:last').type('(303) 303-3030');
+    cy.get('.module-buttons button:first').click();
 
-  //   cy.get('.ContactCard').should('have.length', 5);
-  // });
+    cy.get('.ContactCard').should('have.length', 5);
+  });
 
-  // it('should not add a contact to the contacts list if there are any empty fields', () => {
-  //   cy.get('.ContactCard').should('have.length', 4);
+  it('should not add a contact to the contacts list if there are any empty fields', () => {
+    cy.get('.ContactCard').should('have.length', 4);
 
-  //   cy.get('.add-contacts-button').click();
-  //   cy.get('.add-contacts input:first').type('Gavin');
-  //   cy.get('.add-contacts input').eq(1).type('Belson');
-  //   cy.get('.add-contacts input').eq(2).type('gavin.belson@hooli.com');
-  //   cy.get('.module-buttons button:first').click();
+    cy.get('.add-contacts-button').click();
+    cy.get('.add-contacts input:first').type('Gavin');
+    cy.get('.add-contacts input').eq(1).type('Belson');
+    cy.get('.add-contacts input').eq(2).type('gavin.belson@hooli.com');
+    cy.get('.module-buttons button:first').click();
 
-  //   cy.get('.ContactCard').should('have.length', 4);
-  // });
+    cy.get('.ContactCard').should('have.length', 4);
+  });
 })
