b68f64cd2789d76b24d80f27fb41f1fca2ea78ba
Kristen Bair <kristenmbair@gmail.com>
2021-04-19 16:11:58 -0600

    Add testing for contacts preview display

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -11,4 +11,12 @@ describe('Dashboard', () => {
     cy.get('.NavBar').find('a').should('have.length', 3)
   });
 
+  it('should have a Contacts Preview', () => {
+    cy.get('.ContactsPreview').find('.top-three a').should('have.length', 3)
+    cy.get('.ContactsPreview p').eq(0).should('contain', 'Main Contacts')
+    cy.get('.ContactsPreview').find('.all-contacts').should('contain', 'All Contacts')
+      .click().get('.Contacts').should('be.visible')
+    cy.get('.Header').find('.logo').click()
+  });
+
 });
