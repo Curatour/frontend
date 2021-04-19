fd2ace60371487618634d00f8aa629226b676cfc
Richard Tyler <richardtyler2011@gmail.com>
2021-04-19 11:25:22 -0600

    Test NavBar icons for active classes

--- cypress/integration/navbar_spec.js
+++ cypress/integration/navbar_spec.js
@@ -15,12 +15,32 @@ describe('NavBar', () => {
     cy.get('.NavBar a:first svg').should('have.attr', 'data-icon', 'calendar-alt')
   });
 
+  it('should be visibly inactive when not on the calendar page', () => {
+    cy.get('.NavBar a:first').should('have.class', '')
+  });
+
+  it('should be visibly active when on the calendar page', () => {
+    cy.get('.NavBar a:first').click();
+
+    cy.get('.NavBar a:first').should('have.class', 'active')
+  });
+
   it('should have a link to add an event to the calendar', () => {
     cy.get('.NavBar a').eq(1).should('have.attr', 'href', '/new-event')
   });
 
   it('should have an icon in the add to calendar link', () => {
     cy.get('.NavBar a svg').eq(1).should('have.attr', 'data-icon', 'calendar-plus')
+  });
+
+  it('should be visibly inactive when not on the add events page', () => {
+    cy.get('.NavBar a').eq(1).should('have.class', '')
+  });
+
+  it('should be visibly active when on the calendar page', () => {
+    cy.get('.NavBar a').eq(1).click();
+
+    cy.get('.NavBar a').eq(1).should('have.class', 'active')
   });
 
   it('should have a link to the contacts', () => {
