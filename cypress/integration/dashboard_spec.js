87982d0386c46a3f138ce48d2f659a09966dc676
Kristen Bair <kristenmbair@gmail.com>
2021-04-19 16:11:08 -0600

    Add testing for header display on dashboard

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,29 +1,11 @@
 describe('Dashboard', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/');
-  });
+  before(() => {
+    cy.visit('http://localhost:3000')
+  })
 
   it('should have a header', () => {
-    cy.get('header');
+    cy.get('.Header').find('.logo').should('have.attr', 'src', '/static/media/CuratourLogo.90886745.png')
   });
 
-  it('should have a navbar', () => {
-    cy.get('.NavBar');
-  });
-
-  it('should have a Dashboard component', () => {;
-    cy.get('.Dashboard')
-  });
-
-  it('should have a Contacts Preview with three contacts', () => {
-    // cy.get('.ContactsPreview a').should('have.length', 3);
-  });
-
-  it('should have a Tour Display with the name of the user\'s current tour', () => {
-    cy.get('.TourDisplay > h1')
-  });
-
-  // it.skip('', () => {
-
-  // });
+ 
 });
