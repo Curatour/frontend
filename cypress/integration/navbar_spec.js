517ed782ea222bee184c9f03ceaaccb1131c8125
Richard Tyler <richardtyler2011@gmail.com>
2021-04-19 15:20:15 -0600

    Test NavBar links navigate to their pages

--- cypress/integration/navbar_spec.js
+++ cypress/integration/navbar_spec.js
@@ -11,6 +11,12 @@ describe('NavBar', () => {
     cy.get('.NavBar a:first').should('have.attr', 'href', '/calendar');
   });
 
+  it('should navigate to the calendar page', () => {
+    cy.get('.NavBar a:first').click()
+
+    cy.url().should('contain', '/calendar')
+  });
+
   it('should have an icon in the calendar link', () => {
     cy.get('.NavBar a:first svg').should('have.attr', 'data-icon', 'calendar-alt')
   });
@@ -29,6 +35,12 @@ describe('NavBar', () => {
     cy.get('.NavBar a').eq(1).should('have.attr', 'href', '/new-event')
   });
 
+  it('should navigate to the new-event page', () => {
+    cy.get('.NavBar a').eq(1).click()
+
+    cy.url().should('contain', '/new-event')
+  });
+
   it('should have an icon in the add to calendar link', () => {
     cy.get('.NavBar a svg').eq(1).should('have.attr', 'data-icon', 'calendar-plus')
   });
@@ -45,6 +57,12 @@ describe('NavBar', () => {
 
   it('should have a link to the contacts', () => {
     cy.get('.NavBar a:last').should('have.attr', 'href', '/contacts')
+  });
+
+  it('should navigate to the contacts page', () => {
+    cy.get('.NavBar a:last').click()
+
+    cy.url().should('contain', '/contacts')
   });
 
   it('should have an icon in the add to calendar link', () => {
