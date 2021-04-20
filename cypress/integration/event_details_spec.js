3d70ce6e63047b67a02c2f57134af451acc9c785
Kristen Bair <kristenmbair@gmail.com>
2021-04-20 17:07:38 -0600

    Update event details view

--- cypress/integration/event_details_spec.js
+++ cypress/integration/event_details_spec.js
@@ -1,41 +1,42 @@
-// describe('Event Details', () => {
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000/calendar');
+describe('Event Details', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/calendar');
 
-//     cy.get('.event-link').eq(0).click();
-//   });
+    cy.get('.fc').find('.fc-daygrid-event')
+    .eq(1).click();
+  });
   
-//   it('should be on the event-details page', () => {
-//     cy.url().should('contain', '/event-details');
-//   });
+  it('should be on the event-details page', () => {
+    cy.url().should('contain', '/event-details');
+  });
 
-//   it('should have a visibile event component', () => {
-//     cy.get('.Event').should('be.visible');
-//   });
+  it('should have a visible event component', () => {
+    cy.get('.Event').should('be.visible');
+  });
 
-  // it('should display the current event\'s name', () => {
-  //   cy.get('h1').should('contain', 'some sort of title or something')
-  // }
+  it('should display the current event\'s name', () => {
+    cy.get('h1').should('be.visible')
+  })
 
-  // it('should display the current event\'s venue name', () => {
-  //   cy.get('p:first').should('contain', 'the name of the venue');
-  // });
+  it('should display the current event\'s venue name', () => {
+    cy.get('p:first').should('be.visible');
+  })
 
-  // it('should display the event\'s date', () => {
-  //   cy.get('.date-info').should('contain', 'Date: MM, DD, YYYY')
-  // });
+  it('should display the event\'s date', () => {
+    cy.get('.date-info').should('be.visible')
+  });
 
-  // it('should display an agenda', () => {
-  //   cy.get('.agenda-display').should('be.visible');
-  // });
+  it('should display an agenda', () => {
+    cy.get('.agenda-display').should('be.visible');
+  });
 
-  // it('should have a form to add to the agenda', () => {
-  //   cy.get('.agenda-form').should('be.visible');
-  // });
+  it('should have a form to add to the agenda', () => {
+    cy.get('.agenda-form').should('be.visible');
+  });
 
-  // it('should have some instructions for the form', () => {
-  //   cy.get('.agenda-form h3').should('contain', 'Add to the Agenda');
-  // });
+  it('should have some instructions for the form', () => {
+    cy.get('.agenda-form h3').should('contain', 'Add to the Agenda');
+  });
 
   // it('should have allow the user to input a new event', () => {
   //   cy.get('.agenda-time').type('06:30');
@@ -50,4 +51,4 @@
   //   cy.get('.agenda-item-title').should('contain', '6:30AM - melt faces');
   //   cy.get('.agenda-item-desc').should('contain', 'find faces, melt them');
   // });
-// });
+});
