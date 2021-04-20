e89b4d056d82004c0e3ae12967652d684740e18c
Kristen Bair <kristenmbair@gmail.com>
2021-04-19 21:55:03 -0600

    Remove extra heading, change main header to h1 for accessibility

--- src/components/error/NotFound.js
+++ src/components/error/NotFound.js
@@ -4,8 +4,7 @@ import './NotFound.css';
 const NotFound = () => {
   return (
     <div className='Not-Found'>
-      <h2 className="not-found-heading">Error: Page Not Found</h2>
-      <h3>Sorry!</h3>
+      <h1 className="not-found-heading">Error: Page Not Found</h1>
       <p className='not-found-message'>This is what we get for letting the drummer work on some of this code.</p>
     </div>
   )
