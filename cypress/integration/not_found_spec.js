75aecc32cd9c6d0c5924a551c8384e8c8b1e80bc
Kristen Bair <kristenmbair@gmail.com>
2021-04-19 18:50:30 -0600

    Adjust error component and add tests

--- cypress/integration/not_found_spec.js
+++ cypress/integration/not_found_spec.js
@@ -1,18 +1,14 @@
-// describe('Not Found', () => {
-//   // beforeEach(() => {
-//     // cy.visit('http://localhost:3000/njaosfgoiahsf');
-//   // });
+describe('Not Found', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/njaosfgoiahsf');
+  });
 
-//   it('should have an error message', () => {
-//     cy.get('.not-found-heading').should('contain', 'Error: Page Not Found');
-//   });
+  it('should have an error message', () => {
+    cy.get('.Not-Found').find('.not-found-heading').should('contain', 'This page does not exist! Apologies, please head back to the dashboard.')
+    cy.get('.not-found-message').should('contain', 'This is what we get for letting the drummer work on some of this code.');
+  });
 
-//   it('should apologize', () => {
-//     cy.get('Not-Found h3').should('contain', 'Sorry!');
-//   });
-
-//   it('should explain the ineptitude of drummers', () => {
-//     cy.get('.not-found-message').should('contain', 'This is what we get for letting the drummer work on some of this code.');
-//   });
-
-// });
+  it('should be able to navigate back to the dashboard by clicking the logo in the header', () => {
+    cy.get('.Header').find('.logo').click()
+  })
+});
--- src/components/error/NotFound.js
+++ src/components/error/NotFound.js
@@ -4,8 +4,7 @@ import './NotFound.css';
 const NotFound = () => {
   return (
     <div className='Not-Found'>
-      <h2 className="not-found-heading">Error: Page Not Found</h2>
-      <h3>Sorry!</h3>
+      <h2 className="not-found-heading">This page does not exist! Apologies, please head back to the dashboard.</h2>
       <p className='not-found-message'>This is what we get for letting the drummer work on some of this code.</p>
     </div>
   )
