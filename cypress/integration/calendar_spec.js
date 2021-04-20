32d02048d2e5dbae0447e3062ec4bf9d192060b2
Kristen Bair <kristenmbair@gmail.com>
2021-04-19 18:09:45 -0600

    Test for monthly event view

--- cypress/integration/calendar_spec.js
+++ cypress/integration/calendar_spec.js
@@ -6,6 +6,16 @@ describe('Contacts', () => {
 
   it('should display the Calendar page', () => {
     cy.get('.fc').find('.fc-toolbar-title').should('be.visible')
-  
+    // 'contain', 'April 2021'
+  });
+
+  it('should display events on the monthly calendar view', () => {
+    cy.get('.fc').find('.fc-daygrid-event').should('have.length', 7)
+      .eq(0).should('contain', 'The La\'s')
+    // ~~~~~~~~~~Add in once stubs are fully implemented
+    //   .click()
+    //   .get('.Event').should('be.visible')
+    // cy.get('.NavBar').find('a').eq(0).click()
   })
+ 
 });
