9c840c11774b3700c759346cfc35e027b25bb1b1
Kristen Bair <kristenmbair@gmail.com>
2021-04-20 17:00:44 -0600

    Update contacts tests

--- cypress/integration/contacts_spec.js
+++ cypress/integration/contacts_spec.js
@@ -1,6 +1,8 @@
 describe('Contacts', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/contacts');
+    cy.visit('http://localhost:3000/');
+    cy.get('.NavBar').find('a').eq(2).click()
+
   });
 
   it('should have a contacts area', () => {
@@ -8,35 +10,35 @@ describe('Contacts', () => {
   });
 
   it('should have a list of all contacts', () => {
-    // cy.get('.ContactCard').should('have.length', 4);
+    cy.get('.ContactCard').should('be.visible');
   });
 
   it('should have an icon on each contact card', () => {
-    cy.get('.contact-icons p').should('have.length', 4);
+    cy.get('.contact-icons').find('p').should('be.visible');
   });
 
   it('should have a name on each contact card', () => {
-    cy.get('.contact-name').should('have.length', 4);
+    cy.get('.contact-name').should('be.visible');
   });
 
   it('should have an email on each contact card', () => {
     cy.get('.email')
-      .should('have.length', 4)
+      .should('be.visible')
       .should('have.attr', 'href');
   });
 
   it('should have a phone number on each contact card', () => {
     cy.get('.phone')
-      .should('have.length', 4)
+      .should('be.visible')
       .should('have.attr', 'href');
   });
 
-  it('should have a search bar that can filter displayed contacts', () => {
-    // cy.get('.ContactCard').should('have.length', 4);
+  it('should have a search bar that can filter displayed contacts', () => {    
+    cy.wait(1000)
     
-    // cy.get('.handle-contacts-options input')
-    //   .type('kevin');
+    cy.get('.handle-contacts-options').find('.search-bar')
+      .type('W');
 
-    // cy.get('.ContactCard').should('have.length', 1);
+    cy.get('.ContactCard').should('have.length', 1);
   });
 })
