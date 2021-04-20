cc92730f103c77ccba3cd95d5bbf14b18014d78e
Kristen Bair <kristenmbair@gmail.com>
2021-04-19 18:10:04 -0600

    Add test for weekly calendar view

--- cypress/integration/calendar_spec.js
+++ cypress/integration/calendar_spec.js
@@ -9,13 +9,23 @@ describe('Contacts', () => {
     // 'contain', 'April 2021'
   });
 
-  it('should display events on the monthly calendar view', () => {
+  it('should display events on the monthly calendar view that can be clicked and take user to event details page', () => {
     cy.get('.fc').find('.fc-daygrid-event').should('have.length', 7)
       .eq(0).should('contain', 'The La\'s')
     // ~~~~~~~~~~Add in once stubs are fully implemented
     //   .click()
     //   .get('.Event').should('be.visible')
     // cy.get('.NavBar').find('a').eq(0).click()
-  })
+  });
  
+   it('should be able to show a week view', () => {
+    cy.get('.fc').find('.fc-timeGridWeek-button').click()
+    cy.get('.fc-timeGridWeek-view').should('be.visible')
+    cy.get('.event-link').find('.')
+   .click()
+    //   .get('.Event').should('be.visible')
+    // cy.get('.NavBar').find('a').eq(0).click()
+
+    
+  });
 });
