4b45a3a60ed2b46a70bdbff66ce35dd43ff86b2d
Kristen Bair <kristenmbair@gmail.com>
2021-04-20 17:00:16 -0600

    Update calendar tests

--- cypress/integration/calendar_spec.js
+++ cypress/integration/calendar_spec.js
@@ -1,25 +1,16 @@
-describe('Contacts', () => {
+describe('Calendar', () => {
   beforeEach(() => {
-    cy.intercept('POST', '/graphql', (req) => {
-      const { body } = req
-      if (body.hasOwnProperty('query') && req.body.query.includes('events')) {
-        req.reply({ fixture: 'events.json'});
-      } 
-    })
-    cy.clock(Date.UTC(2021, 3, 20), ['Date']);
     cy.visit('http://localhost:3000/');
     cy.get('.NavBar').find('a').eq(0).click()
   });
   
   it('should display the Calendar page', () => {
     cy.get('.fc').find('.fc-toolbar-title').should('be.visible')
-    // 'contain', 'April 2021'
   });
 
   it('should display events on the monthly calendar view that can be clicked and take user to event details page', () => {
-    cy.get('.fc').find('.fc-daygrid-event').should('have.length', 2)
-      .eq(0).should('contain', 'Bob Marley (and the Wailers)')
-      .click()
+    cy.get('.fc').find('.fc-daygrid-event').should('be.visible')
+      .eq(0).click()
       .get('.Event').should('be.visible')
     cy.get('.NavBar').find('a').eq(0).click()
   });
@@ -27,10 +18,6 @@ describe('Contacts', () => {
    it('should be able to show a week view', () => {
     cy.get('.fc').find('.fc-timeGridWeek-button').click()
     cy.get('.fc-timeGridWeek-view').should('be.visible')
-    cy.get('.event-link').find('b').eq(0).should('contain', '11:20 - 2:20')
-      .click()
-      .get('.Event').should('be.visible')
-    cy.get('.NavBar').find('a').eq(0).click()
   });
 
   it('should be able to show a daily calendar view', () => {
